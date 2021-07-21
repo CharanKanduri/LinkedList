09404a48ac594797ad784c1b1211f1c78668471e
charan_kanduri <charan2000kanduri@gmail.com>
2021-07-21 20:25:15 +0530

    [CHARAN]UC-1

---
+++ LinkedList/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+      public class Node
+        {
+            //Class Node with data and next pointer
+            public int data;
+            public Node next;
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList Operations program");
+            OperationsOnLinkedList obj = new OperationsOnLinkedList();
+            //Insertion Operation
+            obj.InsertLast(56);
+            obj.InsertLast(30);
+            obj.InsertLast(70);
+            //Display Operation
+            Console.WriteLine("*****Insertion at Last*****");
+            obj.Display();
+
+        }
+    }
+}
